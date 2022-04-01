dbedb26d1bcd3772a5965a8b4c4aafeb2b745bb2
George B <705427+georgeblahblah@users.noreply.github.com>
2022-04-01 15:31:09 +0100

    Remove defaultViewport option from launch

--- src/app.ts
+++ src/app.ts
@@ -21,10 +21,6 @@ const getElementViewport = async (
 export const handler = async (url: string) => {
 	const browser = await puppeteer.launch({
 		headless: true,
-		defaultViewport: {
-			width: 670,
-			height: 1000,
-		},
 	});
 
 	const page = await browser.newPage();
